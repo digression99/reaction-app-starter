6066c07dd20e42cb424d09b09b5b69f3d394499a
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-05-25 18:07:08 +0900

    Apply pubsub in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,41 @@
-import React, { useReducer} from 'react';
+import React, { useReducer, useEffect } from 'react';
 import reducer, { initialState } from '../state/reducer';
 import PublishMessage from './PublishMessage';
+import PubSub from '../pubsub';
 import MessageBoard from './MessageBoard';
+import SetUsername from './SetUsername';
+import Context from '../context';
+
+const pubsub = new PubSub();
 
 const App = () => {
   const [ state, dispatch ] = useReducer(reducer, initialState);
 
-  console.log('state : ', state);
+  useEffect(() => {
+    pubsub.addListener({
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        console.log("received message : ", message);
+        console.log("channel : ", channel);
+
+        dispatch(message);
+      }
+    });
+  }, []);
 
   return (
-    <div>
-      <h2>Reaction</h2>
-      <PublishMessage dispatch={dispatch} />
-      <hr/>
-      <MessageBoard messages={state.messages} />
-    </div>
-  )
+    <Context.Provider value={{ state, dispatch, pubsub }}>
+      <div>
+        <h2>Reaction</h2>
+        <SetUsername />
+        <hr />
+        <PublishMessage />
+        <hr />
+        <MessageBoard />
+      </div>
+    </Context.Provider>
+  );
 }
 
 export default App;
