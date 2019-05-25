377759c5b76bc44197ee52031fa7261285bf5bfa
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-05-25 18:06:28 +0900

    Apply set username

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,16 +1,14 @@
-import { NEW_MESSAGE } from './types';
+import { NEW_MESSAGE, SET_USERNAME } from './types';
 
-export const initialState = {
-  messages : []
-};
+export const initialState = { messages : [], username : 'anonymous' };
 
 const reducer = (state, action) => {
+  console.log('state : ', state);
   switch (action.type) {
     case NEW_MESSAGE:
-      return { 
-        ...state, 
-        messages : [...state.messages, action.payload ]
-      };
+      return { ...state, messages : [...state.messages, action.payload ] };
+    case SET_USERNAME:
+      return { ...state, username : action.payload };
     default:
       return state;
   }
