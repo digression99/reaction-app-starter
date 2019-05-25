23fdee6764bd317404316fd9a52b5abd97857177
Pseudocoder Kim <pseudocoder.kim@linecorp.com>
2019-05-25 18:36:08 +0900

    Add MessageBoard

--- src/components/MessageBoard.js
+++ src/components/MessageBoard.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useAppContext } from '../hooks';
+import CreateReaction from './CreateReaction';
+import MessageReactions from './MessageReactions';
 
 export default () => {
-  const { state } = useAppContext();
-  const messages = state.messages;
+  const { state : {messages, reactionsMap} } = useAppContext();
 
   return (
     <div>
       {messages.map(message => {
         const { id, text, username, timestamp } = message;
+
         return (
           <div key={id}>
             <h4>{new Date(timestamp).toLocaleString()}</h4>
             <p>{text}</p>
             <h4>- { username }</h4>
+            <CreateReaction messageId={id} />
+            <MessageReactions messageReactions={reactionsMap[id]}/>
             <hr />
           </div>
         );
